e36376ab9443fdf8d0558f3162cf15886d4b3717
Berkay Atakan İhtiyar <26187453+atakanihtiyar@users.noreply.github.com>
2020-07-02 13:56:12 +0300

    First Fly
    -Add Script
    -Better camera position for seeing flight
    -Fix visual studio auto complete
    -Light yelowish to white

---
+++ Floating/Assets/Scripts/Flight.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flight : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    public float speed;
+    public Vector3 startVelocity;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = startVelocity * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
