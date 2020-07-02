d67fa3c4d436de985674ac560d8d7c9db06c0157
Berkay Atakan İhtiyar <26187453+atakanihtiyar@users.noreply.github.com>
2020-07-02 14:21:47 +0300

    Add Plane Rotate With WASD

--- Floating/Assets/Scripts/Flight.cs
+++ Floating/Assets/Scripts/Flight.cs
@@ -7,17 +7,33 @@ public class Flight : MonoBehaviour
     private Rigidbody rb;
 
     public float speed;
+    public float rotationSpeed;
     public Vector3 startVelocity;
     
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        rb.velocity = startVelocity * speed;
+        rb.velocity = startVelocity;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        if (verticalInput != 0)
+        {
+            verticalInput = verticalInput * speed;
+            transform.Rotate(verticalInput, 0, 0, Space.Self);
+            //rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y + verticalInput, rb.velocity.z);
+        }
+
+        if (horizontalInput != 0)
+        {
+            horizontalInput = horizontalInput * rotationSpeed;
+
+            transform.Rotate(0, horizontalInput, 0, Space.Self);
+        }
     }
 }
