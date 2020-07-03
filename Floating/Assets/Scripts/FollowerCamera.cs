6e08bf4eba419ea91f0fa4e00caafcfe01b3980c
Berkay Atakan İhtiyar <26187453+atakanihtiyar@users.noreply.github.com>
2020-07-03 13:57:56 +0300

    Fix Camera, Convert Collider to Mesh
    -Camera can turn back

--- Floating/Assets/Scripts/FollowerCamera.cs
+++ Floating/Assets/Scripts/FollowerCamera.cs
@@ -7,16 +7,17 @@ public class FollowerCamera : MonoBehaviour
     public Transform target;
 
     public float smoothSpeed = 0.125f;
-    public Vector3 offset;
+    public float offsetYMultiplier;
+    public float offsetZMultiplier;
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        Vector3 offset = (offsetYMultiplier * target.up) - (offsetZMultiplier * target.forward);
         Vector3 desiredPosition = target.position + offset;
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
         transform.position = smoothedPosition;
 
-        transform.rotation = target.rotation;
         transform.LookAt(target);
     }
 }
