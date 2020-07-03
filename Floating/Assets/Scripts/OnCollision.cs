69cf2bb361e04ca05f658756cde0e1b474c3150f
Berkay Atakan İhtiyar <26187453+atakanihtiyar@users.noreply.github.com>
2020-07-03 13:29:08 +0300

    When Collision Add Score

---
+++ Floating/Assets/Scripts/OnCollision.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class OnCollision : MonoBehaviour
+{
+    public int score = 0;
+
+    //When the Primitive collides with the walls, it will reverse direction
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PointCircle"))
+            score++;
+    }
+
+    //When the Primitive exits the collision, it will change Color
+    private void OnTriggerExit(Collider other)
+    {
+
+    }
+}
