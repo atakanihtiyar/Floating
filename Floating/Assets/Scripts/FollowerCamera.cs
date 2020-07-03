5d2f8b551a0c1ab11b2b6ff99fd33c47f14b45bd
Berkay Atakan İhtiyar <26187453+atakanihtiyar@users.noreply.github.com>
2020-07-03 13:04:43 +0300

    Change Materials, Add Point Circle

--- Floating/Assets/Scripts/FollowerCamera.cs
+++ Floating/Assets/Scripts/FollowerCamera.cs
@@ -16,7 +16,6 @@ public class FollowerCamera : MonoBehaviour
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
         transform.position = smoothedPosition;
 
-
         transform.rotation = target.rotation;
         transform.LookAt(target);
     }
