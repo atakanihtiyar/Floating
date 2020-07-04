f2309bca834429c52547e9ab79d3f9cae633d7db
Berkay Atakan İhtiyar <26187453+atakanihtiyar@users.noreply.github.com>
2020-07-04 16:47:05 +0300

    Update Camera
    Barely nothing

--- Floating/Assets/Scripts/FollowerCamera.cs
+++ Floating/Assets/Scripts/FollowerCamera.cs
@@ -7,13 +7,12 @@ public class FollowerCamera : MonoBehaviour
     public Transform target;
 
     public float smoothSpeed = 0.125f;
-    public float offsetYMultiplier;
-    public float offsetZMultiplier;
+    public Vector3 offsetMultiplier;
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        Vector3 offset = (offsetYMultiplier * target.up) - (offsetZMultiplier * target.forward);
+        Vector3 offset = (offsetMultiplier.x * target.right) + (offsetMultiplier.y * target.up) - (offsetMultiplier.z * target.forward);
         Vector3 desiredPosition = target.position + offset;
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
         transform.position = smoothedPosition;
